a2d72484a372fdb7c9d624bba78455a2244102b3
Daniel <danielurreadaza@gmail.com>
2023-02-26 16:32:50 -0500

    v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://daniel1749js.github.io/vite-morty-and-rick/",
+  base: "https://daniel1749js.github.io/rickandmortyapp/",
   plugins: [react()],
 })
